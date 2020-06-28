292bc54a61389a623f0f73c57e1e8944bea94719
Piyush Mahapatra <piyushmahapatra001@gmail.com>
2020-06-28 11:13:16 +0530

    A certificate generator built using PDFLib

---
+++ JSProjects1/certificateGen/script.js
@@ -0,0 +1,68 @@
+const userName = document.getElementById('name');
+const submitBtn = document.getElementById('submitBtn');
+
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener('click', () => {
+  const val = capitalize(userName.value);
+
+  //check if the text is empty or not
+  if (val.trim() !== '' && userName.checkValidity()) {
+    // console.log(val);
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch('./cert.pdf').then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  //get font
+  const fontBytes = await fetch('./Sanchez-Regular.ttf').then((res) =>
+    res.arrayBuffer()
+  );
+
+  // Embed our custom font in the document
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+
+  // Draw a string of text diagonally across the first page
+  firstPage.drawText(name, {
+    x: 300,
+    y: 270,
+    size: 20,
+    font: SanChezFont,
+    color: rgb(0.9, 0.9, 0.9),
+  });
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save();
+  console.log('Done creating');
+
+  // this was for creating uri and showing in iframe
+
+  // const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+  // document.getElementById("pdf").src = pdfDataUri;
+
+  var file = new File([pdfBytes], 'MADTripathy.pdf', {
+    type: 'application/pdf;charset=utf-8',
+  });
+  saveAs(file);
+};
+
+// init();
